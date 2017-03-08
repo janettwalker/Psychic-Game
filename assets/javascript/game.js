9670a2254664170f0a7a383ba38c573ee5f3aea7
Janet Walker <janetwalker@Janets-MBP.attlocal.net>
2017-03-07 20:57:19 -0500

    minor changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -35,46 +35,39 @@ var computerGuess = letterChoices[Math.floor(Math.random() * letterChoices.lengt
 	console.log(computerGuess);
 
 document.onkeyup = function(event) {
-
 	letterGuess = event.key.toLowerCase();
-	document.getElementById("guess-letter").innerHTML = ("Letters Guessed: " + letterGuess);
+		document.getElementById("guess-letter").innerHTML = ("Letters Guessed: " + letterGuess);
 
 if (letterGuess === computerGuess){
-    alert('Great Job, ' + computerGuess + ' was the correct answer.');
-    document.getElementById("total-win").innerHTML = ("Wins: " + wins++);
-    replay();
-	console.log('wins');
+    alert('You win!');
+    	alert('Great Job, ' + computerGuess + ' was the correct answer.');
+   			 document.getElementById("total-win").innerHTML = ("Wins: " + wins++);
+   				 replay();
+					console.log('wins');
 }
 
 else if (letterGuess !== computerGuess){
 	guesses--;
-
-	console.log(guesses);
+		console.log(guesses);
 }
 
 if (guesses === 0){
 	alert('You lose!');
-	alert('The correct answer was ' + computerGuess);
-	document.getElementById("total-loss").innerHTML = ("Losses: " + losses++);
-	replay();
+		alert('The correct answer was ' + computerGuess);
+			document.getElementById("total-loss").innerHTML = ("Losses: " + losses++);
+				replay();
 }
 
 }
 
 var replay = function(){
 	computerGuess = letterChoices[Math.floor(Math.random() * letterChoices.length)];
-	console.log(computerGuess);
+		console.log(computerGuess);
 
 	guesses = 7;
 }
 
 
-
-// var html = "<p>Press any key to start playing!</p>" +
-// "<p>Wins: " + wins + "</p>" +
-// "<p>Losses: " + losses + "</p>";
-
-// document.querySelector("#game").innerHTML = html;
 
 
 
